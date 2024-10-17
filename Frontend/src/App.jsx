3aee2cc14f406d94c0b004cb5478b67c20df6ef0
Piyush Khatri <piyushkhatri968@gmail.com>
2024-10-17 13:28:17 +0500

    configure frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-3xl font-extrabold text-gray-700 text-center mt-7 underline">
+      FYP PROJECT
+    </div>
+  );
+};
+
+export default App;
