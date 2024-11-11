6bd9ec0995f22f65340b627f5bfb0b2b1527714c
Piyush Khatri <piyushkhatri968@gmail.com>
2024-11-12 01:19:54 +0500

    ok

--- Frontend/src/Components/Theme.jsx
+++ Frontend/src/Components/Theme.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Theme = (props) => {
   return (
-    //! For Main name pass the prop as 'pageName' and for the image "heroImage"
+    //? For Main name pass the prop as 'pageName' and for the image "heroImage"
 
     <div className="h-[50vh] sm:h-[60vh] bg-[#070e1f] bg-opacity-80 relative flex justify-center items-center text-white">
       <img
@@ -13,7 +13,7 @@ const Theme = (props) => {
         className="absolute top-0 -z-10 w-[100%] h-[50vh] sm:h-[60vh] object-cover mx-auto"
       />
       {/* Top Design */}
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col items-center gap-6">
         <h1 className="text-4xl md:text-5xl font-bold">{props.pageName}</h1>
         <div className="flex justify-center items-center gap-6 border border-gray-500 max-w-60 py-3 px-4 rounded-full mx-auto">
           <Link
