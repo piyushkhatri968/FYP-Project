0608e065bcdd1bfcbd40d4d518adc23155bce872
Piyush Khatri <piyushkhatri968@gmail.com>
2024-11-23 15:35:12 +0500

    a

--- Backend/Routes/Auth.route.js
+++ Backend/Routes/Auth.route.js
@@ -4,11 +4,8 @@ import { signup, signin, signout } from "../Controller/Auth.controller.js";
 const router = express.Router();
 import { signupValidation } from "../ModelsValidation/signupSchemaValidate.js";
 
-import { createJobPost } from "../Controller/Jobmangement.js";
-
 router.post("/signup", signupValidation, signup);
 router.post("/signin", signin);
 router.post("/signout", signout);
-
 
 export default router;
