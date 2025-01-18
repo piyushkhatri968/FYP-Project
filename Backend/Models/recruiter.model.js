c872f1f4898a828349f4a597579f71475a76a2c2
Pawan kumar <pawankumarmalhi291@gmail.com>
2025-01-18 17:22:13 +0500

    ...

--- Backend/Models/recruiter.model.js
+++ Backend/Models/recruiter.model.js
@@ -6,8 +6,32 @@ const recruiterSchema = new mongoose.Schema({
     ref: "User", // Refers to the User model
     required: true,
   },
-});
+  contactNumber: { 
+    type: String,
+    required: true,
+  },
+  companyName: { 
+    type: String,
+    required: true,
+  },
+  companyAddress: { 
+    type: String,
+    required: true,
+  },
+  department: { 
+    type: String,
+    required: true,
+  },
+  position: { 
+    type: String,
+    required: true,
+  },
+  additionalDetailsFilled: { // To track if the form is completed
+    type: Boolean,
+    default: false,
+  },
+}, { timestamps: true }); // Automatically adds createdAt and updatedAt fields
 
-const Recruiter = new mongoose.model("Recruiter", recruiterSchema);
+const Recruiter = mongoose.model("Recruiter", recruiterSchema);
 
 export default Recruiter;
