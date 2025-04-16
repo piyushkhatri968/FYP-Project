eaca8f2f29a79a0df59c898347068af4ef46968c
Piyush Khatri <piyushkhatri968@gmail.com>
2025-04-16 05:51:37 -0700

    .

--- Frontend/src/Pages/Hr_Dashboard/Components/HrLayout.jsx
+++ Frontend/src/Pages/Hr_Dashboard/Components/HrLayout.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import HRNavbar from "./HrNavbar";
 import HRSidebar from "./HRSidebar";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import HRNavbar from "./HrNavbar.jsx";
 
 const HRLayout = () => {
   return (
