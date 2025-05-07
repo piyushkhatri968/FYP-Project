9bb50b0026f341b19e8084de0d63afad19189222
Piyush Khatri <piyushkhatri968@gmail.com>
2025-05-07 12:32:26 +0500

    .

---
+++ Backend/utils/sendEmail.js
@@ -0,0 +1,27 @@
+import nodeMailer from "nodemailer";
+
+export const sendEmail = async ({ email, subject, message }) => {
+  try {
+    const transporter = nodeMailer.createTransport({
+      host: process.env.SMTP_HOST,
+      service: process.env.SMTP_SERVICE,
+      port: process.env.SMTP_PORT,
+      secure: true,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: "FYP PROJECT",
+      to: email,
+      subject,
+      html: message,
+    };
+
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error("❌ Email sending failed:", error);
+  }
+};
